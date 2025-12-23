305e06e6c9ea511fa4dea7c5e9fb9c2a83dae0a5
FX707VV-I7R4060\Owner <kaoru.ace07@gmail.com>
2025-12-23 23:14:01 +0900

    オープンデータチャレンジの駅のデータを持ってきたよ

--- Map-app/R3A312/map-app/js/main.js
+++ Map-app/R3A312/map-app/js/main.js
@@ -1,6 +1,36 @@
-const map = L.map('map').setView([35.681236, 139.767125], 13);
+// 地図表示
+const map = L.map('map').setView([35.681236, 139.767125], 11);
 
 L.tileLayer(
   'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
   { attribution: '© OpenStreetMap contributors' }
 ).addTo(map);
+
+// ====== ここから駅API ======
+const API_URL =
+  'https://api-challenge.odpt.org/api/v4/odpt:Station' +
+  '?odpt:operator=odpt.Operator:JR-East' +
+  '&acl:consumerKey=fmig1fzk4tflkt40zz4u43z5sg3xrs53jlytlin6numuleq142eqr7ezzxkui28v'; // ←自分のトークン
+
+fetch(API_URL)
+  .then(res => res.json())
+  .then(stations => {
+    stations.forEach(station => {
+      // 緯度・経度（ODPTの形式）
+      const lat = station['geo:lat'];
+      const lng = station['geo:long'];
+
+      // 駅名（日本語）
+      const name = station['odpt:stationTitle']?.ja;
+
+      // 念のためチェック
+      if (!lat || !lng || !name) return;
+
+      L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(name);
+    });
+  })
+  .catch(err => {
+    console.error('駅データ取得エラー', err);
+  });
